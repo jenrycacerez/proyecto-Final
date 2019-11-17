217d8485c142754a1ab5e0f7a762a4a888e2926b
jenry cacerez guzman <jenrybrown@hotmail.com>
2019-11-17 00:45:17 +0000

    prov

--- ProyectoFinal-Jenry/UI/Registros/rProveedor.Designer.cs
+++ ProyectoFinal-Jenry/UI/Registros/rProveedor.Designer.cs
@@ -29,11 +29,276 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.FechadateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TlfmaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.Buscarbutton = new System.Windows.Forms.Button();
+            this.Eliminarbutton = new System.Windows.Forms.Button();
+            this.Nuevobutton = new System.Windows.Forms.Button();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.DirecciontextBox = new System.Windows.Forms.TextBox();
+            this.EmailtextBox = new System.Windows.Forms.TextBox();
+            this.ContactotextBox = new System.Windows.Forms.TextBox();
+            this.NombrestextBox = new System.Windows.Forms.TextBox();
+            this.IDnumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.RnctextBox = new System.Windows.Forms.TextBox();
+            this.MyErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.IDnumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyErrorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FechadateTimePicker
+            // 
+            this.FechadateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.FechadateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.FechadateTimePicker.Location = new System.Drawing.Point(178, 119);
+            this.FechadateTimePicker.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.FechadateTimePicker.Name = "FechadateTimePicker";
+            this.FechadateTimePicker.Size = new System.Drawing.Size(236, 26);
+            this.FechadateTimePicker.TabIndex = 46;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(52, 124);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 20);
+            this.label7.TabIndex = 45;
+            this.label7.Text = "Fecha";
+            // 
+            // TlfmaskedTextBox
+            // 
+            this.TlfmaskedTextBox.Location = new System.Drawing.Point(178, 316);
+            this.TlfmaskedTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TlfmaskedTextBox.Mask = "(999) 000-0000";
+            this.TlfmaskedTextBox.Name = "TlfmaskedTextBox";
+            this.TlfmaskedTextBox.Size = new System.Drawing.Size(236, 26);
+            this.TlfmaskedTextBox.TabIndex = 44;
+            // 
+            // Buscarbutton
+            // 
+            this.Buscarbutton.Location = new System.Drawing.Point(346, 51);
+            this.Buscarbutton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Buscarbutton.Name = "Buscarbutton";
+            this.Buscarbutton.Size = new System.Drawing.Size(68, 58);
+            this.Buscarbutton.TabIndex = 43;
+            this.Buscarbutton.UseVisualStyleBackColor = true;
+            this.Buscarbutton.Click += new System.EventHandler(this.Buscarbutton_Click);
+            // 
+            // Eliminarbutton
+            // 
+            this.Eliminarbutton.Location = new System.Drawing.Point(377, 472);
+            this.Eliminarbutton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Eliminarbutton.Name = "Eliminarbutton";
+            this.Eliminarbutton.Size = new System.Drawing.Size(88, 91);
+            this.Eliminarbutton.TabIndex = 42;
+            this.Eliminarbutton.UseVisualStyleBackColor = true;
+            this.Eliminarbutton.Click += new System.EventHandler(this.Eliminarbutton_Click);
+            // 
+            // Nuevobutton
+            // 
+            this.Nuevobutton.Location = new System.Drawing.Point(38, 472);
+            this.Nuevobutton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Nuevobutton.Name = "Nuevobutton";
+            this.Nuevobutton.Size = new System.Drawing.Size(88, 91);
+            this.Nuevobutton.TabIndex = 41;
+            this.Nuevobutton.UseVisualStyleBackColor = true;
+            this.Nuevobutton.Click += new System.EventHandler(this.Nuevobutton_Click);
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Location = new System.Drawing.Point(223, 472);
+            this.Guardarbutton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(88, 91);
+            this.Guardarbutton.TabIndex = 40;
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            this.Guardarbutton.Click += new System.EventHandler(this.Guardarbutton_Click);
+            // 
+            // DirecciontextBox
+            // 
+            this.DirecciontextBox.Location = new System.Drawing.Point(178, 363);
+            this.DirecciontextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.DirecciontextBox.Name = "DirecciontextBox";
+            this.DirecciontextBox.Size = new System.Drawing.Size(236, 26);
+            this.DirecciontextBox.TabIndex = 39;
+            // 
+            // EmailtextBox
+            // 
+            this.EmailtextBox.Location = new System.Drawing.Point(178, 262);
+            this.EmailtextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.EmailtextBox.Name = "EmailtextBox";
+            this.EmailtextBox.Size = new System.Drawing.Size(236, 26);
+            this.EmailtextBox.TabIndex = 38;
+            // 
+            // ContactotextBox
+            // 
+            this.ContactotextBox.Location = new System.Drawing.Point(178, 210);
+            this.ContactotextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ContactotextBox.Name = "ContactotextBox";
+            this.ContactotextBox.Size = new System.Drawing.Size(236, 26);
+            this.ContactotextBox.TabIndex = 37;
+            // 
+            // NombrestextBox
+            // 
+            this.NombrestextBox.Location = new System.Drawing.Point(178, 165);
+            this.NombrestextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.NombrestextBox.Name = "NombrestextBox";
+            this.NombrestextBox.Size = new System.Drawing.Size(236, 26);
+            this.NombrestextBox.TabIndex = 36;
+            // 
+            // IDnumericUpDown
+            // 
+            this.IDnumericUpDown.Location = new System.Drawing.Point(178, 72);
+            this.IDnumericUpDown.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.IDnumericUpDown.Name = "IDnumericUpDown";
+            this.IDnumericUpDown.Size = new System.Drawing.Size(147, 26);
+            this.IDnumericUpDown.TabIndex = 35;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(52, 319);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 20);
+            this.label6.TabIndex = 34;
+            this.label6.Text = "Telefono";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 265);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 20);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 366);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "Direccion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 213);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Contacto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 168);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Nombres";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 72);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "ID";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(51, 412);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 20);
+            this.label8.TabIndex = 47;
+            this.label8.Text = "RNC";
+            // 
+            // RnctextBox
+            // 
+            this.RnctextBox.Location = new System.Drawing.Point(178, 409);
+            this.RnctextBox.Name = "RnctextBox";
+            this.RnctextBox.Size = new System.Drawing.Size(236, 26);
+            this.RnctextBox.TabIndex = 48;
+            // 
+            // MyErrorProvider
+            // 
+            this.MyErrorProvider.ContainerControl = this;
+            // 
+            // rProveedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(519, 577);
+            this.Controls.Add(this.RnctextBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.FechadateTimePicker);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.TlfmaskedTextBox);
+            this.Controls.Add(this.Buscarbutton);
+            this.Controls.Add(this.Eliminarbutton);
+            this.Controls.Add(this.Nuevobutton);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.DirecciontextBox);
+            this.Controls.Add(this.EmailtextBox);
+            this.Controls.Add(this.ContactotextBox);
+            this.Controls.Add(this.NombrestextBox);
+            this.Controls.Add(this.IDnumericUpDown);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "rProveedor";
             this.Text = "rProveedor";
+            ((System.ComponentModel.ISupportInitialize)(this.IDnumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyErrorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker FechadateTimePicker;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MaskedTextBox TlfmaskedTextBox;
+        private System.Windows.Forms.Button Buscarbutton;
+        private System.Windows.Forms.Button Eliminarbutton;
+        private System.Windows.Forms.Button Nuevobutton;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.TextBox DirecciontextBox;
+        private System.Windows.Forms.TextBox EmailtextBox;
+        private System.Windows.Forms.TextBox ContactotextBox;
+        private System.Windows.Forms.TextBox NombrestextBox;
+        private System.Windows.Forms.NumericUpDown IDnumericUpDown;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox RnctextBox;
+        private System.Windows.Forms.ErrorProvider MyErrorProvider;
     }
 }
--- ProyectoFinal-Jenry/UI/Registros/rProveedor.cs
+++ ProyectoFinal-Jenry/UI/Registros/rProveedor.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ProyectoFinal_Jenry.BLL;
+using ProyectoFinal_Jenry.DAL;
+using ProyectoFinal_Jenry.Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,290 @@ namespace ProyectoFinal_Jenry.UI.Registros
 {
     public partial class rProveedor : Form
     {
-        public rProveedor()
+        private int id;
+        public rProveedor(int id)
         {
+            this.id = id;
             InitializeComponent();
+        }
+
+        private void Limpiar()
+        {
+
+            IDnumericUpDown.Value = 0;
+            FechadateTimePicker.Value = DateTime.Now;
+            NombrestextBox.Text = string.Empty;
+            ContactotextBox.Text = string.Empty;
+            EmailtextBox.Text = string.Empty;
+            TlfmaskedTextBox.Text = string.Empty;
+            DirecciontextBox.Text = string.Empty;
+            RnctextBox.Text = string.Empty;
+            MyErrorProvider.Clear();
+
+        }
+
+        private Proveedor LlenaClase()
+        {
+            Proveedor p = new Proveedor();
+            p.ProveedorId = Convert.ToInt32(IDnumericUpDown.Value);
+            p.Fecha = FechadateTimePicker.Value;
+            p.Nombres = NombrestextBox.Text;
+            p.Contacto = ContactotextBox.Text;
+            p.Email = EmailtextBox.Text;
+            p.Telefono = TlfmaskedTextBox.Text;
+            p.Direccion = DirecciontextBox.Text;
+            p.Rnc = RnctextBox.Text;
+            p.UsuarioId = id;
+            return p;
+        }
+
+        private void LlenaCampo(Proveedor p)
+        {
+            IDnumericUpDown.Value = p.ProveedorId;
+            FechadateTimePicker.Value = p.Fecha;
+            NombrestextBox.Text = p.Nombres;
+            ContactotextBox.Text = p.Contacto;
+            EmailtextBox.Text = p.Email;
+            TlfmaskedTextBox.Text = p.Telefono;
+            DirecciontextBox.Text = p.Direccion;
+            RnctextBox.Text = p.Rnc;
+        }
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            RepositorioBase<Proveedor> Repositorio = new RepositorioBase<Proveedor>();
+            Proveedor p = Repositorio.Buscar((int)IDnumericUpDown.Value);
+            return (p != null);
+        }
+        private void Buscarbutton_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Proveedor> Repositorio = new RepositorioBase<Proveedor>();
+            Proveedor p = new Proveedor();
+            int id;
+            int.TryParse(IDnumericUpDown.Text, out id);
+            Limpiar();
+
+            p = Repositorio.Buscar(id);
+            if (p != null)
+                LlenaCampo(p);
+            else
+                MessageBox.Show("No encontrado.");
+        }
+
+        private void Nuevobutton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+        public static bool RepetirProveedor(string descripcion)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                if (db.Proveedor.Any(p => p.Nombres.Equals(descripcion)))
+                {
+                    paso = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+        public static bool RepetirEmail(string descripcion)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                if (db.Proveedor.Any(p => p.Email.Equals(descripcion)))
+                {
+                    paso = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+        public static bool RepetirTelefono(string descripcion)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                if (db.Proveedor.Any(p => p.Telefono.Equals(descripcion)))
+                {
+                    paso = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+        public static bool RepetirDireccion(string descripcion)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                if (db.Proveedor.Any(p => p.Direccion.Equals(descripcion)))
+                {
+                    paso = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+        public static bool RepetirContacto(string descripcion)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                if (db.Proveedor.Any(p => p.Contacto.Equals(descripcion)))
+                {
+                    paso = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+        private bool Validar()
+        {
+            bool paso = true;
+            MyErrorProvider.Clear();
+            if (string.IsNullOrWhiteSpace(NombrestextBox.Text))
+            {
+                MyErrorProvider.SetError(NombrestextBox, "No puede ser vacio.");
+                paso = false;
+            }
+            if (string.IsNullOrWhiteSpace(EmailtextBox.Text))
+            {
+                MyErrorProvider.SetError(EmailtextBox, "No puede ser vacio.");
+                paso = false;
+            }
+            if (string.IsNullOrWhiteSpace(ContactotextBox.Text))
+            {
+                MyErrorProvider.SetError(ContactotextBox, "No puede ser vacio.");
+                paso = false;
+            }
+            if (string.IsNullOrWhiteSpace(DirecciontextBox.Text))
+            {
+                MyErrorProvider.SetError(DirecciontextBox, "No puede ser vacio.");
+                paso = false;
+            }
+            if (!TlfmaskedTextBox.MaskCompleted)
+            {
+                MyErrorProvider.SetError(TlfmaskedTextBox, "No puede ser vacio.");
+                paso = false;
+            }
+            if (string.IsNullOrWhiteSpace(RnctextBox.Text))
+            {
+                MyErrorProvider.SetError(RnctextBox, "No puede ser vacio.");
+                paso = false;
+            }
+            return paso;
+        }
+        private bool ValidarRepeticion()
+        {
+            bool paso = true;
+            MyErrorProvider.Clear();
+
+            if (RepetirProveedor(NombrestextBox.Text))
+            {
+                MyErrorProvider.SetError(NombrestextBox, "No se pueden repetir.");
+                paso = false;
+            }
+            if (RepetirContacto(ContactotextBox.Text))
+            {
+                MyErrorProvider.SetError(ContactotextBox, "No se pueden repetir.");
+                paso = false;
+            }
+            if (RepetirEmail(EmailtextBox.Text))
+            {
+                MyErrorProvider.SetError(EmailtextBox, "No se pueden repetir.");
+                paso = false;
+            }
+            if (RepetirTelefono(TlfmaskedTextBox.Text))
+            {
+                MyErrorProvider.SetError(TlfmaskedTextBox, "No se pueden repetir.");
+                paso = false;
+            }
+            if (RepetirDireccion(DirecciontextBox.Text))
+            {
+                MyErrorProvider.SetError(DirecciontextBox, "No se pueden repetir.");
+                paso = false;
+            }
+            return paso;
+        }
+
+        private void Guardarbutton_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Proveedor> Repositorio = new RepositorioBase<Proveedor>();
+            Proveedor p = new Proveedor();
+            bool paso = false;
+
+            if (!Validar())
+                return;
+
+
+            p = LlenaClase();
+
+
+            if (IDnumericUpDown.Value == 0)
+            {
+                if (!ValidarRepeticion())
+                    return;
+
+                paso = Repositorio.Guardar(p);
+            }
+            else
+            {
+                if (!ExisteEnLaBaseDeDatos())
+                {
+                    MessageBox.Show("No se puede guardar.", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                paso = Repositorio.Modificar(p);
+            }
+
+            if (paso)
+                MessageBox.Show("Guardado", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("No fue posible guardar", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Limpiar();
+        }
+
+        private void Eliminarbutton_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Proveedor> Repositorio = new RepositorioBase<Proveedor>();
+
+            MyErrorProvider.Clear();
+
+            int id;
+            int.TryParse(IDnumericUpDown.Text, out id);
+
+            Limpiar();
+
+            if (Repositorio.Eliminar(id))
+                MessageBox.Show("Eliminado");
+            else
+                MyErrorProvider.SetError(IDnumericUpDown, "No existe.");
         }
     }
 }
