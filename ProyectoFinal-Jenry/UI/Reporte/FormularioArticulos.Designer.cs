a8e5ac754a90379bb82c7d3d4734d7b2a4b458f5
jenry cacerez guzman <jenrybrown@hotmail.com>
2019-11-18 04:59:52 +0000

    Articulo

---
+++ ProyectoFinal-Jenry/UI/Reporte/FormularioArticulos.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ProyectoFinal_Jenry.UI.Reporte
+{
+    partial class FormularioArticulos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer
+            // 
+            this.crystalReportViewer.ActiveViewIndex = -1;
+            this.crystalReportViewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer.Name = "crystalReportViewer";
+            this.crystalReportViewer.Size = new System.Drawing.Size(959, 622);
+            this.crystalReportViewer.TabIndex = 0;
+            this.crystalReportViewer.Load += new System.EventHandler(this.CrystalReportViewer1_Load);
+            // 
+            // FormularioArticulos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 622);
+            this.Controls.Add(this.crystalReportViewer);
+            this.Name = "FormularioArticulos";
+            this.Text = "FormularioArticulos";
+            this.Load += new System.EventHandler(this.FormularioArticulos_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer;
+    }
+}
---
+++ ProyectoFinal-Jenry/UI/Reporte/FormularioArticulos.cs
@@ -0,0 +1,42 @@
+﻿using ProyectoFinal_Jenry.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal_Jenry.UI.Reporte
+{
+    public partial class FormularioArticulos : Form
+    {
+        private List<Articulos> ListaArticulos;
+        public FormularioArticulos()
+        {
+            InitializeComponent();
+        }
+
+        private void CrystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public FormularioArticulos(List<Articulos> articulos)
+        {
+            this.ListaArticulos = articulos;
+            InitializeComponent();
+        }
+
+        private void FormularioArticulos_Load(object sender, EventArgs e)
+        {
+            rptArticulos oCliente = new rptArticulos();
+            oCliente.SetDataSource(ListaArticulos);
+
+            crystalReportViewer.ReportSource = oCliente;
+            crystalReportViewer.Refresh();
+        }
+    }
+}
