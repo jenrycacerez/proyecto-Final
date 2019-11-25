69e030c7e743ebc61f9871b4d570a73e50a404fc
jenry cacerez guzman <jenrybrown@hotmail.com>
2019-11-25 19:53:26 +0000

    tests

---
+++ BLLTests/VentaBLLTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoFinal_Jenry.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_Jenry.BLL.Tests
+{
+    [TestClass()]
+    public class VentaBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
